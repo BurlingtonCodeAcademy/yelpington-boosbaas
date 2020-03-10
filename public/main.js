fa2f73335003a0989546f657bad4ef0c5b91b636
boosbaas <jbegonya@protonmail.com>
2020-03-10 07:59:46 -0400

    not where I want it to be stylistically

--- public/main.js
+++ public/main.js
@@ -1,20 +1,12 @@
 
-
-
 let mymap = L.map('mapid').setView([44.476264, -73.212883], 16)
 
 
-L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
-    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    subdomains: 'abcd',
-    minZoom: 1,
-    maxZoom: 18,
-    ext: 'jpg'
-}).addTo(mymap);
 L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
-    maxZoom: 18,
+    minZoom: 15,
+    maxZoom: 17,
     attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012',
-id: 'mapbox.streets'
+    id: 'mapbox.streets'
 }).addTo(mymap);
 
 window.fetch('https://json-server.burlingtoncodeacademy.now.sh/restaurants')
@@ -22,21 +14,21 @@ window.fetch('https://json-server.burlingtoncodeacademy.now.sh/restaurants')
     .then((json) => {
         const target = document.getElementById("restaurantList");
 
-        const postList = document.createElement("ol")
+        const postList = document.createElement("ul")
 
         const posts = json.map((postData) => {
             const listItem = document.createElement("li")
-            listItem.textContent = `address:${postData.address}`;
-
-            placeMarker(postData.address, "wtf")
+            let link = listItem.appendChild(document.createElement("a"))
+            link.textContent = `${postData.name}`; //populates list
+            link.href = `${postData.website}` //links to restaurant's website
+            
+            placeMarker(postData)
             return listItem;
 
         });
-        console.log("TEST posts" + posts)
+
         posts.forEach((post) => {
             postList.appendChild(post);
-
-
         })
 
         target.append(postList);
@@ -44,8 +36,8 @@ window.fetch('https://json-server.burlingtoncodeacademy.now.sh/restaurants')
         return undefined;
     })
 
-
-function placeMarker(address, myInfo) {
+function placeMarker(restaurant) {
+    const { name, address, website, phone, hours,notes } = restaurant //destructuring object to make calling easier
     fetch(`https://nominatim.openstreetmap.org/search/?q=${address}&format=json`)
         .then(data => {
             return data.json()
@@ -54,8 +46,12 @@ function placeMarker(address, myInfo) {
             let info = locationInfo[0];
             let lat = info.lat;
             let lon = info.lon;
-            L.marker([lat, lon]).addTo(mymap).bindPopup(myInfo)
-            console.log("TEST" + lat + lon)
+            let display=`${name} ${hours} ${notes}`
+            L.marker([lat, lon]).addTo(mymap).bindPopup(display)
+            
         })
 
 }
+
+
+
